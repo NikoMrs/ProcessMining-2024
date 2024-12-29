6149828fd62295393e26c043b07de00e09a2e6e6
Nicola Mores <51528817+NikoMrs@users.noreply.github.com>
2024-12-29 17:59:40 +0100

    Added second intercase value

--- main.py
+++ main.py
@@ -2,33 +2,33 @@ import pandas as pd
 import pm4py
 from pm4py.objects.conversion.log import converter as log_converter
 
-from sklearn import tree
-from matplotlib import pyplot as plot
-from sklearn import tree
-# from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
-
 def import_xes(file_path):
     log = pm4py.read_xes(file_path)
     log = log_converter.apply(log, variant=log_converter.Variants.TO_EVENT_LOG, parameters={})
     return log
 
+
 def get_label_encoder(log):
-    activity_names=[]
-    for case_index,case in enumerate(log):
-        for event_index,event in enumerate(case):
+    activity_names = []
+    for case_index, case in enumerate(log):
+        for event_index, event in enumerate(case):
             activity_names.append(event['concept:name'])
-    return  {name: idx+1 for idx, name in enumerate(sorted(set(activity_names), key=lambda x: activity_names.index(x)))}
+    return {name: idx + 1 for idx, name in
+            enumerate(sorted(set(activity_names), key=lambda x: activity_names.index(x)))}
+
 
 def print_all_log(log):
     for case_id, case in enumerate(log):
-        print("Case: ", case_id, " ", case.attributes["concept:name"], case[0]["time:timestamp"], "-", case[-1]["time:timestamp"] ,"="*50)
+        print("Case: ", case_id, " ", case.attributes["concept:name"], case[0]["time:timestamp"], "-",
+              case[-1]["time:timestamp"], "=" * 50)
         for event_id, event in enumerate(case):
             print(event_id, " ", event["concept:name"])
+
 
 # TODO encode_case_simple_index (case, prefix_length, label_encoder) se l'attivita' non e' nel mapping possiamo pensare di skippare:
 # Compute the Simple Index encoding and cut the encoding at a specific prefix_length,
 # if trace_length < prefix_length 0's are added
-def encode_case_simple_index (case, prefix_length, label_encoder):
+def encode_case_simple_index(case, prefix_length, label_encoder):
     encoded_case = []
     for event_id, event in enumerate(case):
         encoded_event_name = label_encoder[event["concept:name"]]
@@ -38,28 +38,14 @@ def encode_case_simple_index (case, prefix_length, label_encoder):
     trace.append(case.attributes["label"])
     return trace
 
+
 # TODO decode_case_simple_index (forse)
 
 # TODO count_concurrent_cases (case, log):
-def count_concurrent_cases (case, log):
-
-    # min = case[0]["time:timestamp"]
-    # max = case[0]["time:timestamp"]
-    # max_id = 0
-    # min_id = 0
-    # for idx, activity in enumerate(case):
-    #     if (activity["time:timestamp"] >= max):
-    #         max = activity["time:timestamp"]
-    #         max_id = idx
-    #     if (activity["time:timestamp"] < min):
-    #         min = activity["time:timestamp"]
-    #         min_id = idx
-    # print(max_id == (len(case) -1))
-    # print(max_id, " - ", min_id, len(case))
-
+def count_concurrent_cases(case, log):
     start_timestamp = case[0]["time:timestamp"]
     end_timestamp = case[-1]["time:timestamp"]
-    # print(start_timestamp, " - ", end_timestamp)
+
     intersecting_traces = pm4py.filter_time_range(
         log,
         start_timestamp,
@@ -73,10 +59,10 @@ def count_concurrent_cases (case, log):
 
 
 # TODO count_avg_duration (case, log):
-def count_avg_duration (case, log):
+def count_avg_duration(case, log):
     start_timestamp = case[0]["time:timestamp"]
     end_timestamp = case[-1]["time:timestamp"]
-    # print(start_timestamp, " - ", end_timestamp)
+
     intersecting_traces = pm4py.filter_time_range(
         log,
         start_timestamp,
@@ -85,24 +71,22 @@ def count_avg_duration (case, log):
         case_id_key='concept:name',
         timestamp_key='time:timestamp'
     )
-
-    # print(intersecting_traces[1])
 
     # Compute avg duration
     cumulative_time = 0
     for case_id, case in enumerate(intersecting_traces):
-        # time = datetime(case[-1]["time:timestamp"]) - datetime(case[0]["time:timestamp"])
         time = (pd.Timedelta(case[-1]["time:timestamp"] - case[0]["time:timestamp"])).total_seconds()
         cumulative_time += time
-        # TODO se la traccia ha una sola attivita' allora possiamo guardare la durata della singola attivita', il problema e' che non so in  che unita' di misura e; espressa
+        # TODO se la traccia ha una sola attività allora possiamo guardare la durata della singola attività, il problema è che non so in che unita' di misura è espressa
         # dur = 0
         # for event_id, event in enumerate(case):
         #     dur += int(event["activity_duration"])
         # print(time, " - ", dur)
-    return round(cumulative_time/len(intersecting_traces))
+    return round(cumulative_time / len(intersecting_traces))
+
 
 # TODO count_my_intercase_value: (dobbiamo scegliere una metrica)
-def count_avg_resources_concurrent_cases (case, log):
+def count_avg_resources_concurrent_cases(case, log):
     start_timestamp = case[0]["time:timestamp"]
     end_timestamp = case[-1]["time:timestamp"]
 
@@ -114,11 +98,6 @@ def count_avg_resources_concurrent_cases (case, log):
         case_id_key='concept:name',
         timestamp_key='time:timestamp'
     )
-
-    # resources = set()
-    # for event_id, event in enumerate(log[2]):
-    #     resources.add(event["Resource"])
-    # print("PLUTO: ", len(resources), resources)
 
     cumulative_resources = 0
     for case_id, case in enumerate(intersecting_traces):
@@ -126,10 +105,41 @@ def count_avg_resources_concurrent_cases (case, log):
         for event_id, event in enumerate(case):
             resources.add(event["Resource"])
         cumulative_resources += len(resources)
-    return round(cumulative_resources/len(log), 2)
+    return round(cumulative_resources / len(log), 2)
+
+
+def count_avg_overlapping_duration_concurrent_cases(case, log):
+    start_timestamp = case[0]["time:timestamp"]
+    end_timestamp = case[-1]["time:timestamp"]
+
+    intersecting_traces = pm4py.filter_time_range(
+        log,
+        start_timestamp,
+        end_timestamp,
+        mode='traces_intersecting',
+        case_id_key='concept:name',
+        timestamp_key='time:timestamp'
+    )
+
+    overlapping_durations = []
+
+    for case in intersecting_traces:
+        case_start = case[0]["time:timestamp"]
+        case_end = case[-1]["time:timestamp"]
+
+        overlap_start = max(start_timestamp, case_start)
+        overlap_end = min(end_timestamp, case_end)
+
+        if (overlap_start < overlap_end):
+            duration = pd.Timedelta(overlap_end - overlap_start).total_seconds()
+            overlapping_durations.append(duration)
+
+    return round((sum(overlapping_durations) / len(overlapping_durations)), 2)
+
 
 # TODO simple_index_encode (log, prefix_length, label_encoder, conc_cases, avg_dur, my_int) come codifico i nuovi valori ottenuti dalle trasformazioni (potremmo aggiungerli al label_encoder):
-def simple_index_encode (log, prefix_length, label_encoder, conc_cases, avg_dur, my_int):
+def simple_index_encode(log, prefix_length, label_encoder, conc_cases=False, avg_dur=False, my_int1=False,
+                        my_int2=False):
     encode_result = []
     for case_id, case in enumerate(log):
         base_encode = encode_case_simple_index(case, prefix_length, label_encoder)
@@ -140,13 +150,19 @@ def simple_index_encode (log, prefix_length, label_encoder, conc_cases, avg_dur,
             base_encode.insert(-1, n)
         if (avg_dur):
             # Compute avg duration of concurrent traces
-            n = round(count_avg_duration(case, log)/(3600*24), 2)
+            n = round(count_avg_duration(case, log) / (3600 * 24), 2)
             base_encode.insert(-1, n)
-        if (my_int):
+        if (my_int1):
+            # Compute avg number of resources used by concurrent traces
             n = count_avg_resources_concurrent_cases(case, log)
+            base_encode.insert(-1, n)
+        if (my_int2):
+            # Compute avg overlapping time duration of concurrent traces
+            n = round(count_avg_overlapping_duration_concurrent_cases(case, log) / (3600 * 24), 2)
             base_encode.insert(-1, n)
 
         encode_result.append(base_encode)
+
     return pd.DataFrame(data=encode_result)
 
 
@@ -160,9 +176,8 @@ if __name__ == '__main__':
     # print(count_avg_duration(log[0], log), " sec")
     # print(round(count_avg_duration(log[0], log)/(3600*24), 2), " days")
     # print(count_avg_resources_concurrent_cases(log[2], log))
+    # print(round(count_avg_overlapping_duration_concurrent_cases(log[2], log)/(3600*24), 2), " days")
 
     # training_set = simple_index_encode(log, 5, label_encoder, conc_cases=True, avg_dur=True, my_int=True)
     training_set = simple_index_encode(log, 5, label_encoder, conc_cases=False, avg_dur=True, my_int=True)
-
-
-
+    print(training_set)
