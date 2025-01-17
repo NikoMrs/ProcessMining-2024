b9c8e579067485767b19034783e75df30bd9325f
Nicola Mores <51528817+NikoMrs@users.noreply.github.com>
2025-01-17 15:56:58 +0100

    Added comments

--- encoding.py
+++ encoding.py
@@ -2,12 +2,13 @@ import pandas as pd
 import pm4py
 from pm4py.objects.conversion.log import converter as log_converter
 
+# Function used to import a xes file, given its path
 def import_xes(file_path):
     log = pm4py.read_xes(file_path)
     log = log_converter.apply(log, variant=log_converter.Variants.TO_EVENT_LOG, parameters={})
     return log
 
-
+# Function used to create a label encoder
 def get_label_encoder(log):
     activity_names = []
     for case_index, case in enumerate(log):
@@ -16,7 +17,7 @@ def get_label_encoder(log):
     return {name: idx + 1 for idx, name in
             enumerate(sorted(set(activity_names), key=lambda x: activity_names.index(x)))}
 
-
+# Function used to print the entire log
 def print_all_log(log):
     for case_id, case in enumerate(log):
         print("Case: ", case_id, " ", case.attributes["concept:name"], case[0]["time:timestamp"], "-",
@@ -24,10 +25,8 @@ def print_all_log(log):
         for event_id, event in enumerate(case):
             print(event_id, " ", event["concept:name"])
 
-
 # TODO encode_case_simple_index (case, prefix_length, label_encoder) se l'attivita' non e' nel mapping possiamo pensare di skippare:
-# Compute the Simple Index encoding and cut the encoding at a specific prefix_length,
-# if trace_length < prefix_length 0's are added
+# Function used to encode a trace using simple index encoding
 def encode_case_simple_index(case, prefix_length, label_encoder):
     encoded_case = []
     for event_id, event in enumerate(case):
@@ -38,10 +37,11 @@ def encode_case_simple_index(case, prefix_length, label_encoder):
     trace.append(case.attributes["label"])
     return trace
 
-
 # TODO decode_case_simple_index (forse)
 
+
 # TODO count_concurrent_cases (case, log):
+# Function used to compute the number of cases in the event log that were executed concurrently with the given trace
 def count_concurrent_cases(case, log):
     start_timestamp = case[0]["time:timestamp"]
     end_timestamp = case[-1]["time:timestamp"]
@@ -57,8 +57,8 @@ def count_concurrent_cases(case, log):
 
     return len(intersecting_traces)
 
-
 # TODO count_avg_duration (case, log):
+# Function used to compute the average duration of the cases in the event log that were exectued concurrently with the given trace
 def count_avg_duration(case, log):
     start_timestamp = case[0]["time:timestamp"]
     end_timestamp = case[-1]["time:timestamp"]
@@ -84,8 +84,8 @@ def count_avg_duration(case, log):
         # print(time, " - ", dur)
     return round(cumulative_time / len(intersecting_traces))
 
-
 # TODO count_my_intercase_value: (dobbiamo scegliere una metrica)
+# Function used to compute the average amount of unique resources involved in the cases that were executed concurrently with the given trace
 def count_avg_resources_concurrent_cases(case, log):
     start_timestamp = case[0]["time:timestamp"]
     end_timestamp = case[-1]["time:timestamp"]
@@ -107,7 +107,7 @@ def count_avg_resources_concurrent_cases(case, log):
         cumulative_resources += len(resources)
     return round(cumulative_resources / len(log), 2)
 
-
+# Function used to compute the average overlapping duration between the given trace and all other traces that are exectured concurrently
 def count_avg_overlapping_duration_concurrent_cases(case, log):
     start_timestamp = case[0]["time:timestamp"]
     end_timestamp = case[-1]["time:timestamp"]
@@ -141,6 +141,7 @@ def count_avg_overlapping_duration_concurrent_cases(case, log):
 
     return retval
 
+# Function used to compute the average number of concurrent cases for each event within the given trace
 def count_avg_concurrent_cases_per_event(case, log):
     concurrent_counts = []
 
@@ -166,8 +167,8 @@ def count_avg_concurrent_cases_per_event(case, log):
 
     return retval
 
-
 # TODO simple_index_encode (log, prefix_length, label_encoder, conc_cases, avg_dur, my_int) come codifico i nuovi valori ottenuti dalle trasformazioni (potremmo aggiungerli al label_encoder):
+# Function used to encode the given log using Simple index encoding, with optional intercase features
 def simple_index_encode(log, prefix_length, label_encoder, conc_cases=False, avg_dur=False, my_int1=False,
                         my_int2=False, my_int3=False):
     encode_result = []
--- prediction_model.py
+++ prediction_model.py
@@ -13,15 +13,17 @@ import random
 
 from encoding import *
 
-
+# Function used to train a decision model using an encoded dataset
 def train_dt(model, encoded_data:pd.DataFrame):
     model.fit(encoded_data.iloc[:, :-1], encoded_data.iloc[:, -1:])
     return model
 
+# Function used to generate predictions for a dataset using a given decision tree
 def predict(model:tree.DecisionTreeClassifier, encoded_data:pd.DataFrame):
     predictions = model.predict(encoded_data.iloc[:, :-1])
     return predictions
 
+# Function used to compute and return key metrics for a set of predictions
 def get_metrics(predictions, gold_standard):
     metrics = {
         'accuracy': accuracy_score(gold_standard.iloc[:, -1:], predictions),
@@ -32,6 +34,7 @@ def get_metrics(predictions, gold_standard):
 
     return metrics
 
+# Function used for hyperparameter optimization
 def model_optimization(encoded_data:pd.DataFrame, max_evals=1000):
 
     param_space = {
@@ -60,22 +63,26 @@ def model_optimization(encoded_data:pd.DataFrame, max_evals=1000):
     return best, trials
 
 if __name__ == '__main__':
+
     PREFIX_LENGTH = 5
 
     training_set = import_xes("./Production_avg_dur_training_0-80.xes")
     test_set = import_xes("./Production_avg_dur_testing_80-100.xes")
 
-    # Compute the label_encoder looking at the activity of the training set
+    # Compute the label_encoder based on the training set
     label_encoder = get_label_encoder(training_set)
 
+    # Define the combinations of intercase features
     feature_names = ["Concurrent_cases", "Average_duration", "Average_resources", "Overlapping_duration",
                      "Concurrent_cases_per_event"]
     combinations = list(product([False, True], repeat=len(feature_names)))
 
-    STATE_SEED = 123456
+    # Define a Seed to be used in the creation of the Decision Tree
+    # STATE_SEED = 123456
     STATE_SEED = random.randint(0, 4294967295)
-    current_time = datetime.now()
 
+    # Define a header string used in the log file
+    current_time = datetime.now()
     header_string = f"{current_time.strftime('%d/%m/%Y %H:%M:%S')} - SEED {STATE_SEED}"
     padding_length = (119 - len(header_string) - 2) // 2
     line = f"{'-' * padding_length} {header_string} {'-' * padding_length}"
@@ -83,12 +90,16 @@ if __name__ == '__main__':
 
     best_results = None
     best_combination = None
-    with open("./results.log", "a") as f:
+
+    # Train-Test of the Decision Tree
+    with open("results.log", "a") as f:
 
         f.write(f"{line}\n\n\n")
 
+        # Iterate over all possible combinations of intercase features
         for combination in combinations:
-            print(combination)
+
+            # print(combination)
             conc_cases, avg_dur, my_int1, my_int2, my_int3 = combination
 
             # Training
@@ -108,11 +119,12 @@ if __name__ == '__main__':
             features_used = [feature for feature, included in zip(feature_names, combination) if included]
             features_description = "Features: " + ", ".join(features_used) if features_used else "Features: None"
 
-            # Write results on file
+            # Write results on log file
             f.write(f"{features_description}\n")
             f.write(f"Result: Accuracy = {metrics['accuracy']:.4f}, Precision = {metrics['precision']:.4f}, "
                     f"Recall = {metrics['recall']:.4f}, F1-score = {metrics['f1_score']:.4f}\n\n")
 
+            # Keep track of the combination producing the best metrics
             if best_combination is None:
                 best_combination = combination
                 best_results = metrics
@@ -120,49 +132,11 @@ if __name__ == '__main__':
                 best_combination = combination
                 best_results = metrics
 
+        # Write the best combination/metrics on log file
         features_used = [feature for feature, included in zip(feature_names, best_combination) if included]
         features_description = ", ".join(features_used) if features_used else "Features: None"
         f.write(f"BEST Combination: {features_description}\n")
         f.write(f"BEST Result: Accuracy = {best_results['accuracy']:.4f}, Precision = {best_results['precision']:.4f}, "
                 f"Recall = {best_results['recall']:.4f}, F1-score = {best_results['f1_score']:.4f}\n\n")
-        f.write(f"{'-' * 119}\n\n")
 
-    # conc_cases = False
-    # avg_dur = False
-    # my_int1 = False
-    # my_int2 = False
-    # my_int3 = True
-    #
-    # # Perform the encoding af the training set
-    # encoded_training_set = simple_index_encode(training_set, PREFIX_LENGTH, label_encoder, conc_cases=conc_cases,
-    #                                            avg_dur=avg_dur, my_int1=my_int1, my_int2=my_int2, my_int3=my_int3)
-    #
-    # # Define the decision tree with the preferred parameters
-    # model = DecisionTreeClassifier(
-    #     criterion='gini',
-    #     # max_depth=9,
-    #     # max_features=110,
-    #     # min_samples_leaf=3,
-    #     # min_samples_split=4,
-    #     # max_leaf_nodes=47,
-    #     # random_state=17493,
-    # )
-    #
-    # # Perform the training of the model
-    # model = train_dt(model, encoded_training_set)
-    #
-    # # Perform the encoding af the test set
-    # encoded_test_set = simple_index_encode(test_set, PREFIX_LENGTH, label_encoder, conc_cases=conc_cases,
-    #                                        avg_dur=avg_dur, my_int1=my_int1, my_int2=my_int2, my_int3=my_int3)
-    #
-    # # Predict the outcome for the test set
-    # predictions = predict(model, encoded_test_set)
-    #
-    # # Evaluate the prediction's result
-    # print_metrics(predictions, encoded_test_set)
-    #
-    # # Plot the decision tree created during the training process
-    # # tree.plot_tree(model, fontsize=8)
-    # # plot.show()
-    #
-    # # model_optimization(encoded_training_set, max_evals=50000)
+        f.write(f"{'-' * 119}\n\n")
